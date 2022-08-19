9cc146b9fcfb6f4e53f82bcc27f06ce96d7e94c0
ryanl <ryanlimadev@gmail.com>
2022-08-19 12:08:25 -0300

    finalizando projeto

--- app/src/main/java/com/rlds/playmusic/MainActivity.java
+++ app/src/main/java/com/rlds/playmusic/MainActivity.java
@@ -2,13 +2,75 @@ package com.rlds.playmusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageViewPause, imageViewPlay, imageViewStop;
+    private MediaPlayer mediaPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageViewPause = findViewById(R.id.pause);
+        imageViewPlay = findViewById(R.id.play);
+        imageViewStop = findViewById(R.id.stop);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.global);
+
+        imageViewPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaPlayer != null){
+                    mediaPlayer.start();
+
+                }
+
+            }
+        });
+        imageViewPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaPlayer != null){
+                    mediaPlayer.pause();
+
+                }
+
+            }
+        });
+        imageViewStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaPlayer != null){
+                    mediaPlayer.stop();
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.global);
+
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer!= null && mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+
+        }
     }
 }
